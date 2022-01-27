d6115f5e937be7af49d5937b0955fdc5f75b49f7
sammyon <samamandre.mandracon@gmail.com>
2022-01-27 13:57:48 +0700

    need to insert session

--- LivinHood/routes/index.js
+++ LivinHood/routes/index.js
@@ -1,6 +1,7 @@
 const Controller = require ('../controllers')
 const express = require('express')
 const router = express.Router()
+const session = require('express-session')
 const admin = require('./admin.js')
 const user = require('./user.js')
 const login = require('./login.js')
@@ -13,7 +14,9 @@ router.get ('/login', Controller.loginForm)
 router.post ('/login', Controller.login)
 
 router.use(function (res, req, next) {
-
+  if (condition) {
+    next()
+  }
 })
 
 // router.get ('/login', login) //LOGIN
